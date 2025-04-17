4b2741491751f968c303f121f8e169b546ebd67e
Hamza <hamza.boudradar@gmail.com>
2025-04-17 15:41:10 +0200

    Setup project with Tailwind & Eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,47 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { fixupConfigRules } from "@eslint/compat";
 import { FlatCompat } from "@eslint/eslintrc";
+import pluginJs from "@eslint/js";
+import hooksPlugin from "eslint-plugin-react-hooks";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import tailwind from "eslint-plugin-tailwindcss";
+import globals from "globals";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const compat = new FlatCompat();
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  // Tailwind
+  ...tailwind.configs["flat/recommended"],
+  // React
+  ...fixupConfigRules(pluginReactConfig),
+  {
+    plugins: {
+      "react-hooks": hooksPlugin,
+    },
+    rules: hooksPlugin.configs.recommended.rules,
+  },
+  // NextJS
+  {
+    ignores: [".next/"],
+  },
+  ...fixupConfigRules(
+    compat.extends("plugin:@next/next/core-web-vitals, plugin:@next/typescript")
+  ),
+  // Rules configS
+  {
+    rules: {
+      "react/react-in-jsx-scope": 0,
+      "react/no-unescaped-entities": 0,
+      "react/prop-types": 0,
+    },
+  },
+  // Ignore files
+  {
+    ignores: ["tailwind.config.ts", "next.config.ts", "*.ts"],
+  },
 ];
 
 export default eslintConfig;
